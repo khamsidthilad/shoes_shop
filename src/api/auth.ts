1f81d1048a72ea36a06e357789bde5efaed506ec
devSTL <khamsidthilad4@gmail.com>
2025-04-26 16:21:22 +0700

    add register

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,9 @@
 import { api } from "../lib/interceptor";
-import { ILogin } from "../types/admin/auth";
+import { ILogin, IRegister } from "../types/admin/auth";
 
 export default {
   login: (payload: ILogin) => api.post("/auth/login", payload),
-  register: (payload: string) => api.post("/auth/register"),
+  register: (payload: IRegister) => api.post("/auth/register", payload),
   getMe: () => api.get("/auth/me"),
   createAdmin: (payload: string) => api.post("/admin/register"),
 };
